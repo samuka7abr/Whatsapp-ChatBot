67b9e874c3bba974e7fe5ec3323f4e185da30ba8
samuka7abr <samuelabrao2006@gmail.com>
2025-03-21 13:19:41 -0300

    feat: uso do axios pra tratamento de erros da api

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -1,5 +1,22 @@
-exports.sendMessage = (messageData) => {
+const axios = require('axios');
+const config = require('../config');
 
-    console.log('Sending message:', messageData);
-  };
-  
+/**
+ * @param {string} recipient 
+ * @param {string} text 
+ * @returns {Promise<Object>} 
+ */
+exports.sendMessage = async (recipient, text) => {
+  try {
+    const response = await axios.post(`${config.whatsappApiUrl}/send`, {
+      to: recipient,
+      message: text,
+      apiKey: config.whatsappApiKey
+    });
+    console.log("Message sent successfully:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error sending message:", error.message);
+    throw error;
+  }
+};
